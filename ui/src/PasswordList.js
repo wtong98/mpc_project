96114eea4f82766a39c911ad1b28c5000f1df750
wtong98 <williamtong105@gmail.com>
2021-11-14 15:33:29 -0500

    Add list of accounts

--- ui/src/PasswordList.js
+++ ui/src/PasswordList.js
@@ -4,53 +4,41 @@
 
 import * as React from 'react';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
-import InboxIcon from '@mui/icons-material/Inbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
+import LanguageIcon from '@mui/icons-material/Language';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import { usePasswordList } from './request';
 
 export default function PasswordList() {
-  return (
-    <Box sx={{ width: '100%', bgcolor: 'background.paper', mt: '1rem' }}>
-      <nav aria-label="main mailbox folders">
-        <List>
-          <ListItem disablePadding>
+    const passwordList = usePasswordList();
+    const listItems = passwordList.map((entry) => (
+        <ListItem disablePadding key={entry.pid}>
             <ListItemButton>
-              <ListItemIcon>
-                <InboxIcon />
-              </ListItemIcon>
-              <ListItemText primary="Inbox" />
+                <ListItemIcon>
+                    <LanguageIcon />
+                </ListItemIcon>
+                <ListItemText primary={entry.account} />
+                <IconButton>
+                    <DeleteOutlineIcon />
+                </IconButton>
             </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <DraftsIcon />
-              </ListItemIcon>
-              <ListItemText primary="Drafts" />
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </nav>
-      <Divider />
-      <nav aria-label="secondary mailbox folders">
-        <List>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemText primary="Trash" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton component="a" href="#simple-list">
-              <ListItemText primary="Spam" />
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </nav>
-    </Box>
-  );
+        </ListItem>
+    ));
+
+    return (
+        <Box sx={{ width: '100%', bgcolor: 'background.paper', mt: '1rem' }}>
+            <List>
+                {listItems}
+            </List>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Button variant="contained">Add</Button>
+            </div>
+        </Box>
+    );
 }
---
+++ ui/src/request.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
+const localServerUrl = 'localhost:36287';
+
+export function usePasswordList() {
+    const [passwordList, setPasswordList] = useState([]);
+
+    useEffect(() => {
+        async function getPasswords() {
+            // const resp = await axios.get(localServerUrl + '/list')
+            // setPasswordList(resp.data)
+            const dummyPasswords = [
+                {
+                    pid: 0,
+                    account: 'Netflix',
+                    username: 'CouchPoetaetoe',
+                },
+                {
+                    pid: 1,
+                    account: 'Steam',
+                    username: 'PuppyCh0w'
+                },
+                {
+                    pid: 2,
+                    account: 'Email',
+                    username: 'crypto.anarchist@protonmail.com'
+                }
+            ]
+
+            setPasswordList(dummyPasswords)
+        }
+
+        getPasswords();
+    }, []);
+
+    return passwordList;
+}
+
+export async function addPassword(data) {
+    const resp = await axios.post(localServerUrl + '/add', data);
+    return resp;
+}
+
+export function usePasswordDecrypt(pid) {
+
+}
+
+export function deleteAccount(pid) {
+
+}
